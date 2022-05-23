cd8073fb5f11710a23ae6c6e39bb0c9c4a83e37d
Kancabricot <dodge28032000@gmail.com>
2022-05-23 16:13:30 +0200

    add hook encore

--- src/scenes/TestRoom.ts
+++ src/scenes/TestRoom.ts
@@ -1,4 +1,4 @@
-import { Vector } from 'matter'
+import { Body, Vector } from 'matter'
 import Phaser from 'phaser'
 import ObstaclesController from './ObstaclesController'
 import PlayerController from './PlayerController'
@@ -10,6 +10,12 @@ export default class TestRoom extends Phaser.Scene
     private screenHeight
     private numberOfBoxes = 5
     private hero
+    private hook
+    private rope
+    
+    private WALL = 'WALL'
+    private BALL = 'BALL'
+    private HOOK = 'HOOK'
 
     constructor()
     {
@@ -23,7 +29,7 @@ export default class TestRoom extends Phaser.Scene
 
     preload()
     {
-
+        this.load.atlas('penguin', 'assets/penguin.png', 'assets/penguin.json')
     }
 
     create()
@@ -48,15 +54,37 @@ export default class TestRoom extends Phaser.Scene
             let poly = this.matter.add.rectangle(posX, posY, width, height, {
                 isStatic: true
             });
-            poly.label = 'WALL';
+            poly.label = this.WALL
         }
 
         // adding a bouncing ball labeled as BALL
-        this.hero = this.matter.add.rectangle(this.screenWidth / 2, this.screenHeight / 2, 16, 16, {
+        this.hero = this.matter.add.rectangle(this.screenWidth / 2, this.screenHeight / 2, 16, 16,{
             restitution: 0.5
         });
-        this.hero.label = 'BALL';
+        this.hero.setStatic(true)
+        this.hero.label = this.BALL;
+        //this.matter.body.setStatic(box, true)
 
+        this.hook = null
+
+        //this.input.on("pointerdown", this.fireHook, this)
+
+        this.rope = null
+
+        let me = this
+        /*this.matter.world.on("collisionstart", function(e, b1, b2){
+            if((b1.label = me.BALL) || (b2.label = me.BALL))
+            {
+                me.releaseHook()
+                return
+            }
+
+            if((b1.label == me.HOOK) || (b2.label = me.HOOK) && !me.rope)
+            {
+                // make the hook static
+                me.hook.static = true
+            }
+        })*/
     }
 
     destroy()
@@ -67,5 +95,6 @@ export default class TestRoom extends Phaser.Scene
     update(t: number, dt: number)
     {
 
+        console.log(this.hero.isStatic)
     }
 }
